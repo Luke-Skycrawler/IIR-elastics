18d1c2922adc523326e2b0d913562f7fe94c60d2
shay <shay@utah.edu>
2024-11-05 17:14:29 -0700

    tets also have twist (with stvk?)

--- geometry_rod.py
+++ geometry_rod.py
@@ -58,8 +58,8 @@ class Rod:
         b = np.zeros((n_unknowns), np.float32)
         self.a.fill(0.0)
         # self.bulk_kernel(b)
-        # self.tet_kernel(b)
-        self.fill_hessian()
+        self.tet_kernel()
+        # self.fill_hessian()
 
     def get_A1(self, N):
         V0 = self.V0
@@ -156,23 +156,6 @@ class Rod:
 
     def export_tobj(self):
         export_tobj("bar2.tobj", self.V0, self.T.to_numpy())
-
-    @ti.kernel
-    def tet_kernel(self, b: ti.types. ndarray()):
-        for e in range(n_tets):
-            F = ti.Matrix.identity(ti.f32, 3)
-
-            for _i in range(4):
-                for _j in range(4):
-
-                    i = self.T[e, _i]
-                    j = self.T[e, _j]
-                    H = self.compute_Hij(F, _i, _j) @ self.Dm[e]
-                    
-                    volume = self.compute_volume_tet(e)
-                    for k, l in ti.static(ti.ndrange(3, 3)):
-                        # self.a[i * 3 + k, j * 3 + l] += H[k, l] * volume
-                        self.a[i * 3 + k, j * 3 + l] += H[k, l] * volume
 
     def compute_dFdx(self, Dm):
 
@@ -286,7 +269,70 @@ class Rod:
             self.Dm[e] = inv_Dm
 
             
+    @ti.kernel
+    def tet_kernel(self): 
+        for e in range(n_tets):
+            
+            x = self.xq_tet(e)
+            for _i in range(4):
+                i = self.T[e, _i]
+                
+                bi, dbidx = self.bf_tet(e, _i, x)
 
+                for _j in range(4):
+                    j = self.T[e, _j]
+                    bj, dbjdx = self.bf_tet(e, _j, x)
+                    for k in ti.static(range(3)):
+                        grad_v = ti.Vector.unit(3, k, ti.f32).outer_product(dbidx)
+                        eps = (grad_v + grad_v.transpose()) / 2
+                        c = eps.trace() * lam * dbjdx + 2 * mu * eps.transpose() @ dbjdx
+                        for l in ti.static(range(3)):
+                            self.a[i * 3 + k, j * 3 + l] += c[l] * volume
+
+    @ti.func
+    def bf_tet(self, e, _i, x):
+        bi = 0.25
+        n = self.normal(e, _i)
+        x0 = self.xcs[self.T[e, _i]]
+        k = 0.75 / ((x0 - x).dot(n))
+        dbidx = n * k
+        
+        return bi, dbidx
+
+    @ti.func
+    def normal(self, e, _i):
+        v0 = self.T[e, 0]
+        v1 = self.T[e, 1]
+        v2 = self.T[e, 2]
+
+        v = self.T[e, _i]
+        x = self.xcs[v]
+        if _i == 0:
+            v0 = self.T[e, 3]
+        if _i == 1:
+            v1 = self.T[e, 3]
+        if _i == 2:
+            v2 = self.T[e, 3]
+
+        x0 = self.xcs[v0]
+        x1 = self.xcs[v1]
+        x2 = self.xcs[v2]
+
+        n = (x1 - x0).cross(x2 - x0).normalized()
+        if n.dot(x - x0) < 0:
+            n = -n
+        return n
+    
+
+    @ti.func
+    def xq_tet(self, e):
+        x0 = self.xcs[self.T[e, 0]]
+        x1 = self.xcs[self.T[e, 1]]
+        x2 = self.xcs[self.T[e, 2]]
+        x3 = self.xcs[self.T[e, 3]]
+
+        return 0.25 * (x0 + x1 + x2 + x3)
+            
     @ti.kernel
     def bulk_kernel(self, b: ti.types.ndarray()):
         for e in range(n_elements):
