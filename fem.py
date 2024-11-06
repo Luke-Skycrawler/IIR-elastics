ddea53c3b4e1bcba7eb09abbba9b1e226c959f40
shay <shay@utah.edu>
2024-11-06 09:56:06 -0700

    wrap dangling kernels into class QuadFEM

--- fem.py
+++ fem.py
@@ -144,7 +144,7 @@ class QuadFEM:
     def bulk_kernel(self, b: ti.types.ndarray()):
         for e in range(n_elements):
             for iq in range(nq):
-                x = xq(e, iq)
+                x = QuadFEM.xq(e, iq)
                 for _i in range(8):
                     i = self.nodes[e, _i]
 
@@ -152,18 +152,18 @@ class QuadFEM:
                     #     # filter out boundary nodes
                     #     continue
 
-                    bi, dbidx = bf_trilinear(i, x)
+                    bi, dbidx = QuadFEM.bf_trilinear(i, x)
                     # grad_v = ti.Matrix.cols([dbidx, dbidx, dbidx])
                     # grad_v = (grad_v + grad_v.transpose()) / 2
 
-                    f = fx(x)
+                    f = QuadFEM.fx(x)
                     for k in ti.static(range(3)):
                         b[i * 3 + k] += f[k] * bi * wq * volume
                         # \int fv dx
                         
                     for _j in range(8):
                         j = self.nodes[e, _j]
-                        bj, dbjdx = bf_trilinear(j, x)
+                        bj, dbjdx = QuadFEM.bf_trilinear(j, x)
                         for k in ti.static(range(3)):
                             # fill in by row
 
@@ -175,151 +175,159 @@ class QuadFEM:
                                 self.a[i * 3 + k, j * 3 + l] += c[l] * wq * volume
 
 
+    @staticmethod
+    @ti.func
+    def xc(I):
+        '''
+        coord of nodes
+        '''
+        i = I // ((n_yz + 1) ** 2)
+        i_yz = I % ((n_yz + 1) ** 2)
+        j = i_yz // (n_yz + 1)
+        k = i_yz % (n_yz + 1)
 
-@ti.func
-def xc(I):
-    '''
-    coord of nodes
-    '''
-    i = I // ((n_yz + 1) ** 2)
-    i_yz = I % ((n_yz + 1) ** 2)
-    j = i_yz // (n_yz + 1)
-    k = i_yz % (n_yz + 1)
+        x = ti.Vector([i, j, k], ti.f32)
+        return dx * x
 
-    x = ti.Vector([i, j, k], ti.f32)
-    return dx * x
+    @staticmethod
+    @ti.func
+    def trans(I):
+        i, j, k = I // 4, (I % 4) // 2, I % 2
+        return (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
 
-@ti.func
-def trans(I):
-    i, j, k = I // 4, (I % 4) // 2, I % 2
-    return (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
+    @staticmethod
+    @ti.func
+    def _trans(I):
+        i = I // ((n_yz) ** 2)
+        i_yz = I % ((n_yz) ** 2)
+        j = i_yz // (n_yz)
+        k = i_yz % (n_yz)
+        return (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
 
-@ti.func
-def _trans(I):
-    i = I // ((n_yz) ** 2)
-    i_yz = I % ((n_yz) ** 2)
-    j = i_yz // (n_yz)
-    k = i_yz % (n_yz)
-    return (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
+    @staticmethod
+    @ti.func
+    def bf_trilinear(i, x):
+        # FIXME: add divergence
+        u = x - QuadFEM.xc(i)
+        bi = 0.
+        dbidx = ti.Vector.zero(ti.f32, 3)
+        abs_u = ti.abs(u)
+        distance = ti.max(abs_u[0], abs_u[1], abs_u[2])
+        if distance < dx:
+            # so that excludes divided by zero 
+            t = ti.abs(dx - abs_u)
+            bi = t[0] * t[1] * t[2] / (dx ** 3)
 
-@ti.func
-def bf_trilinear(i, x):
-    # FIXME: add divergence
-    u = x - xc(i)
-    bi = 0.
-    dbidx = ti.Vector.zero(ti.f32, 3)
-    abs_u = ti.abs(u)
-    distance = ti.max(abs_u[0], abs_u[1], abs_u[2])
-    if distance < dx:
-        # so that excludes divided by zero 
-        t = ti.abs(dx - abs_u)
-        bi = t[0] * t[1] * t[2] / (dx ** 3)
+            for k in ti.static(range(3)):
+                if u[k] > 0:
+                    dbidx[k] = - bi / (dx - u[k])
+                elif u[k] <= 0:
+                    dbidx[k] = bi / (u[k] + dx)
+        return bi, dbidx
 
+    # @ti.kernel 
+    # def quadrature():
+    #     '''
+    #     8-pt quadrature
+    #     '''
+    #     a = 0.5773502692
+
+    # @ti.kernel
+    # def test():
+    #     x = xq(0, 0)
+    #     print(bf_trilinear(0, x))
+
+    @staticmethod
+    @ti.func
+    def xq(e, I):
+        a = 0.5773502692
+        J = ti.Vector([I // 4, (I % 4) // 2, I % 2], ti.i32)
+        x0 = QuadFEM.xc(QuadFEM._trans(e))
+        x = x0
         for k in ti.static(range(3)):
-            if u[k] > 0:
-                dbidx[k] = - bi / (dx - u[k])
-            elif u[k] <= 0:
-                dbidx[k] = bi / (u[k] + dx)
-    return bi, dbidx
+            x[k] += ((1 - a) / 2 + a * J[k]) * dx 
+        return x
 
-# @ti.kernel 
-# def quadrature():
-#     '''
-#     8-pt quadrature
-#     '''
-#     a = 0.5773502692
+    @staticmethod
+    @ti.func
+    def xq_2d(e, I):
+        a = 0.5773502692
+        x0 = QuadFEM.xc(QuadFEM.boundary_nodes(e, 0))
+        xi = QuadFEM.xc(QuadFEM.boundary_nodes(e, I))
+        x = x0 + dx * (1 - a) / 2 + (xi - x0) * a
+        return x
 
-# @ti.kernel
-# def test():
-#     x = xq(0, 0)
-#     print(bf_trilinear(0, x))
+    @staticmethod
+    @ti.func
+    def boundary_nodes(e, I):
+        '''
+        e: boundary element (square) index
+        I: node index within element
+        '''
+        side = e // (n_yz * n_x)
+        t = e % (n_yz * n_x)
+        i = t // n_yz
+        j = 0
+        k = 0
+        if side == 0 or side == 2:
+            k = t % n_yz
+            j = 0 if side == 0 else n_yz
+        else:
+            j = t % n_yz
+            k = 0 if side == 1 else n_yz
 
-@ti.func
-def xq(e, I):
-    a = 0.5773502692
-    J = ti.Vector([I // 4, (I % 4) // 2, I % 2], ti.i32)
-    x0 = xc(_trans(e))
-    x = x0
-    for k in ti.static(range(3)):
-        x[k] += ((1 - a) / 2 + a * J[k]) * dx 
-    return x
+        # i, j, k: index of the bottom left 
+        bl = (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
+        di = (I // 2) * (n_yz + 1) ** 2
+        djj = 1 if side == 0 or side == 2 else (n_yz + 1)
+        dj = (I % 2) * djj
+        return bl + di + dj
 
-@ti.func
-def xq_2d(e, I):
-    a = 0.5773502692
-    x0 = xc(boundary_nodes(e, 0))
-    xi = xc(boundary_nodes(e, I))
-    x = x0 + dx * (1 - a) / 2 + (xi - x0) * a
-    return x
+    @staticmethod
+    @ti.func
+    def normal(e):
+        side = e // (n_yz * n_x)
+        ny = ti.Vector([0.0, 1.0, 0.0])
+        nz = ti.Vector([0.0, 0.0, 1.0])
+        n = ti.Vector.zero(ti.f32, 3)
+        if side == 0:
+            # bottom
+            n = -ny
+        elif side == 1:
+            # left
+            n = -nz
+        elif side == 2:
+            # top
+            n = ny
+        elif side == 3:
+            # right 
+            n = nz        
+        return n
 
-@ti.func
-def boundary_nodes(e, I):
-    '''
-    e: boundary element (square) index
-    I: node index within element
-    '''
-    side = e // (n_yz * n_x)
-    t = e % (n_yz * n_x)
-    i = t // n_yz
-    j = 0
-    k = 0
-    if side == 0 or side == 2:
-        k = t % n_yz
-        j = 0 if side == 0 else n_yz
-    else:
-        j = t % n_yz
-        k = 0 if side == 1 else n_yz
+    @staticmethod
+    @ti.func
+    def fx(x):
+        return -rho * g * ti.Vector.unit(3, 1, ti.f32)
 
-    # i, j, k: index of the bottom left 
-    bl = (n_yz + 1) ** 2 * i + (n_yz + 1) * j + k 
-    di = (I // 2) * (n_yz + 1) ** 2
-    djj = 1 if side == 0 or side == 2 else (n_yz + 1)
-    dj = (I % 2) * djj
-    return bl + di + dj
+            
 
-@ti.func
-def normal(e):
-    side = e // (n_yz * n_x)
-    ny = ti.Vector([0.0, 1.0, 0.0])
-    nz = ti.Vector([0.0, 0.0, 1.0])
-    n = ti.Vector.zero(ti.f32, 3)
-    if side == 0:
-        # bottom
-        n = -ny
-    elif side == 1:
-        # left
-        n = -nz
-    elif side == 2:
-        # top
-        n = ny
-    elif side == 3:
-        # right 
-        n = nz        
-    return n
-
-@ti.func
-def fx(x):
-    return -rho * g * ti.Vector.unit(3, 1, ti.f32)
-
-        
-
-
-@ti.kernel
-def boundary_kernel(b: ti.types.ndarray()):
-    for e in range(n_boundary_elements):
-        for iq in range(4):
-            x = xq_2d(e, iq)
-            for _i in range(4):
-                i = boundary_nodes(e, _i)
-                if i < (n_yz + 1) ** 2:
-                    continue
-                bi, dbidx = bf_trilinear(i, x)
-                v = ti.Vector([bi, bi, bi])
-                
-                for _j in range(4):
-                    j = boundary_nodes(e, _j)
-                    n = normal(e)
-                    bj, dbjdx = bf_trilinear(j, x)
-                    sigma = lam * dbidx.dot(ti.Vector([1., 1., 1.], ti.f32)) * ti.Matrix.identity(ti.f32, 3) + mu * (ti.Matrix.cols([dbjdx, dbjdx, dbjdx]) + ti.Matrix.rows([dbjdx, dbjdx, dbjdx]))
-                    rhs = (n.transpose() @ sigma @ v * area * wq_2d)[0, 0]
-                    b[i] += rhs
+    @staticmethod
+    @ti.kernel
+    def boundary_kernel(b: ti.types.ndarray()):
+        for e in range(n_boundary_elements):
+            for iq in range(4):
+                x = QuadFEM.xq_2d(e, iq)
+                for _i in range(4):
+                    i = QuadFEM.boundary_nodes(e, _i)
+                    if i < (n_yz + 1) ** 2:
+                        continue
+                    bi, dbidx = QuadFEM.bf_trilinear(i, x)
+                    v = ti.Vector([bi, bi, bi])
+                    
+                    for _j in range(4):
+                        j = QuadFEM.boundary_nodes(e, _j)
+                        n = QuadFEM.normal(e)
+                        bj, dbjdx = QuadFEM.bf_trilinear(j, x)
+                        sigma = lam * dbidx.dot(ti.Vector([1., 1., 1.], ti.f32)) * ti.Matrix.identity(ti.f32, 3) + mu * (ti.Matrix.cols([dbjdx, dbjdx, dbjdx]) + ti.Matrix.rows([dbjdx, dbjdx, dbjdx]))
+                        rhs = (n.transpose() @ sigma @ v * area * wq_2d)[0, 0]
+                        b[i] += rhs
